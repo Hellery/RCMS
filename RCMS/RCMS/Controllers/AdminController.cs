986b34c7c1919ab2f1279cc2e1f9a961a4ad7915
rocher <rocher@126.com>
2016-08-05 23:56:40 +0800

    Platform framework Found

---
+++ RCMS/RCMS/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RCMS.Controllers
+{
+    /// <summary>
+    /// Base-Class for platform
+    /// </summary>
+    public class AdminController : Controller
+    {
+      
+    }
+}
