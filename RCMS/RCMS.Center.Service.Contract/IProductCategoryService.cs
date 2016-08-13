ce90ae25a5835a53335fd49f847522aec1a08290
rocher <rocher@126.com>
2016-08-13 17:16:58 +0800

    ProductCategory Api

--- RCMS/RCMS.Center.Service.Contract/IProductCategoryService.cs
+++ RCMS/RCMS.Center.Service.Contract/IProductCategoryService.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace RCMS.Center.Service.Contract
 {
-    public class Class1
+    public interface IProductCategoryService
     {
+        
     }
 }
---
+++ RCMS/RCMS.Center.Service/Controllers/ProductCategoryController.cs
@@ -0,0 +1,14 @@
+﻿using RCMS.Center.Service.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RCMS.Center.Service.Controllers
+{
+    public class ProductCategoryController : ApiController,IProductCategoryService
+    {
+    }
+}
