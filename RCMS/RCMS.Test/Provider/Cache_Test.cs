0d94713e77326ffa99817a614aa18905dd83ef70
rocher <rocher@126.com>
2016-09-01 22:07:04 +0800

    cache provider.

---
+++ RCMS/RCMS.Test/Provider/Cache_Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFramework.Infrastructure.Const;
+using RFramework.Cache.Interface;
+using RFramework.Provider;
+using RCMS.Public.Entity;
+
+namespace RCMS.Test.Provider
+{
+    [TestClass]
+    public class Cache_Test : TestBase
+    {
+        public CacheProvider CacheProvider
+        {
+            get
+            {
+                return BaseProvider.LoadInstance<CacheProvider>("RedisCacheProvider");
+            }
+        }
+
+        [TestMethod]
+        public void CacheProvider_Test()
+        {
+            string CacheKey = RFramework.Infrastructure.Const.CacheKey.GetApp(800002);
+            CacheProvider.Add<T_App>(CacheKey, new T_App
+            {
+                AppId=800002,
+                AppSecret= "88B2A59E-E1FB-4A0B-ABEB-485ACB316549",
+                AppName="后台",
+                AppStatus=1
+            });
+            Assert.IsTrue(true);
+        }
+    }
+}
