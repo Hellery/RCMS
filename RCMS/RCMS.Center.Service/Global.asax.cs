3f693f6be3b31cce47389a4351d6fbb3cc04d39e
rocher <rocher@126.com>
2016-09-01 20:50:23 +0800

    Make pagepage works.

--- RCMS/RCMS.Center.Service/Global.asax.cs
+++ RCMS/RCMS.Center.Service/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace RCMS.Center.Service
@@ -11,6 +12,8 @@ namespace RCMS.Center.Service
     {
         protected void Application_Start()
         {
+            //HelpPage 需要区域注册
+            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
